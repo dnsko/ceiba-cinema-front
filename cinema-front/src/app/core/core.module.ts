41dc1398a4f4df3a0153382a8eec4360ce837532
Diego Escobar <diego.escobar@ceiba.com.co>
2019-02-25 13:40:52 -0500

    Ajuste

---
+++ cinema-front/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+@NgModule({
+    imports: [
+    ],
+    declarations: [],
+    providers: [
+    ],
+    exports: []
+  })
+  export class CoreModule {
+  }
